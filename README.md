a19b38419fb39e040600514d487402006d140f3a
Esteban Campos Granados <estebandcg1999@gmail.com>
2021-10-07 18:56:54 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Hackaton - Juventudes Rurales
+
+# Python packages to install (on an env)
+
+- flask
+- flask-mysql
---
+++ flask_server/flaskr/__init__.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, request, session, redirect, url_for
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
+
+    @app.route('/')
+    def index():
+        if 'username' in session:
+            return f'Logged in as {session["username"]}'
+        return 'You are not logged in'
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        return '''
+            <form method="post">
+                <p><input type=text name=username>
+                <p><input type=submit value=Login>
+            </form>
+        '''
+
+    @app.route('/logout')
+    def logout():
+        session.pop('username', None)
+        return redirect(url_for('index'))
+
+    return app
