7d8f87cb1f221a2dcfa0d90a2d483abc02c06e8e
tsg3 <estebandcg1999@gmail.com>
2021-10-10 00:56:18 -0600

    SQL Database (#2)
    * sql database script
    
    * clean script for the database
    
    * [feat] db controller and new project layout

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,25 @@
 import os
 from flask import Flask, request, session, redirect, url_for
+import flaskr.app.persistence.users as users
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    path = os.path.dirname(__file__)
+    path = os.path.abspath(os.path.join(path, "../config/data"))
+    file = open(path, "r")
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY=file.read()
     )
+    file.close()
 
     @app.route('/')
     def index():
+        roles = users.get_roles()
+        # roles = "roles"
         if 'username' in session:
-            return f'Logged in as {session["username"]}'
-        return 'You are not logged in'
+            return f'Logged in as {session["username"]} -> roles={roles}'
+        return f'You are not logged in -> roles={roles}'
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
---
+++ flaskr/app/persistence/db.py
@@ -0,0 +1,14 @@
+import pymysql
+import pymysql
+import os
+
+def db_connect():
+    path = os.path.dirname(__file__)
+    path = os.path.abspath(os.path.join(path, "db_config"))
+    file = open(path, "r")
+    connection = pymysql.connect(host='localhost',
+        user='quipu',
+        password=file.read(),
+        db='quipu_db')
+    file.close()
+    return connection
